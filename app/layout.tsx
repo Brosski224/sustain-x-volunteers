5c718ea659c56822281e057a5aaa11ba8f06d8a0
Midhun <midhun122015@gmail.com>
2025-01-31 22:58:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'SustainX Volenteer',
+  description: 'A volenteer platform for SustainX', 
 }
 
 export default function RootLayout({
