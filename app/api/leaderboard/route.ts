e9ab76d4af26c73f98ef4b1507c37ec0d1d1fe9e
Midhun <midhun122015@gmail.com>
2025-01-31 23:52:41 +0530

    Revert "search"
    This reverts commit 6fd7c62f088e1db7a0739340ca3904e44a7a48ca.

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
   try {
     await connectDB();
 
-    const users = await User.find({}, 'name department section emailCount ticketSold points');
+    const users = await User.find();
 
     const leaderboardData = users.map((user) => ({
       name: user.name,
